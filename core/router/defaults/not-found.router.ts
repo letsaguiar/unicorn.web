4f5539529e71d80dd12451f85d725dd858425f43
Letícia Aguiar <letsaguiar@yandex.com>
2023-09-23 00:10:10 -0300

    remove response

--- core/router/defaults/not-found.router.ts
+++ core/router/defaults/not-found.router.ts
@@ -1,9 +1,6 @@
 import { HttpMethod, HttpStatus } from "../../http/http.enum";
-import { ResponseBuilder } from "../../response/response.builder";
 import { Router } from "../router.entity";
 
 export const NotFoundRouter = new Router('*', HttpMethod.ALL, () => {
-	return new ResponseBuilder()
-		.setStatus(HttpStatus.NOT_FOUND)
-		.build();
+	return new Response(null, { status: HttpStatus.NOT_FOUND })
 })
